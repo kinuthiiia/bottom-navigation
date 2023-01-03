4caee1bd99bef7a2b603ed2a323bbfca2b9dce8c
Stephen Kinuthia <skinuthia800@gmail.com>
2023-01-03 16:58:52 +0300

    Pruned unused deps

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,9 @@
 import "../styles/globals.css";
-import { createClient } from "@supabase/supabase-js";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 import { MantineProvider, createEmotionCache } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
 
 function MyApp({ Component, pageProps }) {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-  const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
   const myCache = createEmotionCache({
     key: "mantine",
     prepend: false,
